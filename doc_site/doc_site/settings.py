1d607b93245f56925fe70d46a6e2573a5be73f17
Tyler Schwehr <Tylerschwehr04@gmail.com>
2025-10-04 22:59:32 +0800

    fixed error

--- doc_site/doc_site/settings.py
+++ doc_site/doc_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h#s3bxx*myhud=5rhkz!^$q&_9*z(#2fbfgwqzyqagg=gtgv_t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
